68c77fcd0fb59aded487eb074bd98a0a9e79cd20
smallwoodj19 <smallwoodj19@students.ecu.edu>
2022-04-28 11:59:56 -0400

    something

--- Mycanvas.js
+++ Mycanvas.js
@@ -13,27 +13,6 @@ let lineWidth = 5;
 let startX;
 let startY;
 
-//database connect//
-//var pg = require(pg);
-
-//var connectionString = "postgres://postgres:password@javascriptionary/12.0.0.1:5432/jaiciecongress";
-
-//var pgClient = new pg.Client(connectionString);
-
-//pgClient.connect();
-
-// example query
-//var query = pgClient.query("select * from words");
-
-// adding row found in query to a result 
-//query.on("row", function(row,result){
-
-    //result.addRow(row);
-   // });
-
-
-//console.log("done");
-
 
 toolbar.addEventListener('click', e => {
     if (e.target.id === 'clear') {
@@ -79,7 +58,27 @@ canvas.addEventListener('mouseup', e => {
 canvas.addEventListener('mousemove', draw);
 
 
+const { Client, Query } = require('pg')
+const client = new Client({
+    user: "postgres",
+    password: "Password",
+    host: "127.0.0.1",
+    port: 5432,
+    database: "jaiciecongress"
+
+})
 
 
+client.connect()
+.then(() => console.log("connected successfuly"))
+.then(() => client.query("select word from words where wordid = %s;" % getRandomIntInclusive))
+.then(results => console.table(results.rows))
+.catch(e => console.log(e))
+.finally(() => client.end())
 
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
